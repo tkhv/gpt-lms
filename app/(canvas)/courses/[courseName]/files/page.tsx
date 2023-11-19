03d98895ecec2b4ffff26e589471353b946c5d4e
Thottempudi, Harsha <harsha.02k@gmail.com>
2023-11-18 21:02:16 -0500

    Added table to list files

--- app/(canvas)/courses/[courseName]/files/page.tsx
+++ app/(canvas)/courses/[courseName]/files/page.tsx
@@ -1,6 +1,39 @@
 "use client";
 import { useState } from "react";
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 export default function Files({ params }: { params: { courseName: string } }) {
-  return <div className="flex">course {params.courseName} Files </div>;
+  return (
+    <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="flex items-center justify-between space-y-2">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">
+            {params.courseName} Files
+          </h2>
+        </div>
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">File Name</TableHead>
+            <TableHead className="text-right"></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">testC.c</TableCell>
+            <TableCell className="text-right">add delete btn</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
