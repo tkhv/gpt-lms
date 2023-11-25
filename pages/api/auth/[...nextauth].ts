5a94fdec29970d2557d043a5590fa89458c0853f
Thottempudi, Harsha <harsha.02k@gmail.com>
2023-11-25 17:40:49 -0500

    Implemented DB connection pooling

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,22 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import PostgresAdapter from "@auth/pg-adapter";
-const { Pool } = require("pg");
-
-// Store user accounts in CosmosDB Postgres as well
-const pool = new Pool({
-  max: 300,
-  connectionTimeoutMillis: 5000,
-  host: process.env.AZURE_COSMOSDB_PG_URL,
-  port: 5432,
-  user: process.env.AZURE_COSMOSDB_PG_USER,
-  password: process.env.AZURE_COSMOSDB_PG_PASSWORD,
-  database: process.env.AZURE_COSMOSDB_PG_DBNAME,
-  ssl: true,
-});
+import pool from "@/data/dbPool";
 
 export const options = {
-  adapter: PostgresAdapter(pool),
+  adapter: PostgresAdapter(pool), // Use CosmosDB PG for session storage
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || "NOT_SET",
--- pages/api/refreshDB.ts
+++ pages/api/refreshDB.ts
@@ -1,18 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-const { Pool } = require("pg");
-
-import { refreshDBQuery } from "@/queries/pgQueries";
-
-const pool = new Pool({
-  max: 300,
-  connectionTimeoutMillis: 5000,
-  host: process.env.AZURE_COSMOSDB_PG_URL,
-  port: 5432,
-  user: process.env.AZURE_COSMOSDB_PG_USER,
-  password: process.env.AZURE_COSMOSDB_PG_PASSWORD,
-  database: process.env.AZURE_COSMOSDB_PG_DBNAME,
-  ssl: true,
-});
+import { refreshDBQuery } from "@/data/pgQueries";
+import pool from "@/data/dbPool";
 
 async function refreshDB() {
   try {
