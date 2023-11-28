a9b1728bb774346d2e5fb44db90e47b2886ef75b
Junhaeng <jlee3571@gatech.edu>
2023-11-28 13:33:36 -0500

    resolving courseName issue

--- pages/[courseName]/index.tsx
+++ pages/[courseName]/index.tsx
@@ -14,39 +14,39 @@ export default function Course({ courseName }: CourseProps) {
 the possible options for the dynamic route. We fetch 
 the courseList for the logged in user and pre-render 
 those. */
-export const getStaticPaths: GetStaticPaths = (async () => {
-  // Call our API to get courseList for this user.
-  // This is dummy data
-  const courseList = [
-    { name: "cs2200" },
-    { name: "cs3312" },
-    { name: "cs4400" },
-  ];
+// export const getStaticPaths: GetStaticPaths = (async () => {
+//   // Call our API to get courseList for this user.
+//   // This is dummy data
+//   const courseList = [
+//     { name: "cs2200" },
+//     { name: "cs3312" },
+//     { name: "cs4400" },
+//   ];
 
-  const paths = courseList.map((course) => ({
-    params: { courseName: course.name },
-  }));
+//   const paths = courseList.map((course) => ({
+//     params: { courseName: course.name },
+//   }));
 
-  // { fallback: false } means other routes should 404.
-  return {
-    paths: paths,
-    fallback: false,
-  };
-}) satisfies GetStaticPaths;
+//   // { fallback: false } means other routes should 404.
+//   return {
+//     paths: paths,
+//     fallback: false,
+//   };
+// }) satisfies GetStaticPaths;
 
-// pre-render this page at build time
-export const getStaticProps: GetStaticProps<CourseProps> = async ({
-  params,
-}) => {
-  if (!params || typeof params.courseName !== "string") {
-    return {
-      notFound: true,
-    };
-  }
+// // pre-render this page at build time
+// export const getStaticProps: GetStaticProps<CourseProps> = async ({
+//   params,
+// }) => {
+//   if (!params || typeof params.courseName !== "string") {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  const courseName = params.courseName;
-  return { props: { courseName } };
-};
+//   const courseName = params.courseName;
+//   return { props: { courseName } };
+// };
 
 // Course.getLayout = function (page: React.ReactNode) {
 //   return <Layout>{page}</Layout>;
