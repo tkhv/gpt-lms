230ba6542f1cd754867d49b8050896e5b8d12b04
Junhaeng <jlee3571@gatech.edu>
2023-11-28 13:18:13 -0500

    modified sideabar code

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -24,40 +24,41 @@ const Sidebar: FC = () => {
     // Otherwise, compare against the lowercased item
     return paramList[2] === it.toLocaleLowerCase();
   };
-  useEffect(() => {
-    // If currentCourse is not set, redirect to the home page
-    if (!currentCourse) {
-      router.push("/");
-    }
-  }, [currentCourse, router]);
+  // useEffect(() => {
+  //   // If currentCourse is not set, redirect to the home page
+  //   if (!currentCourse) {
+  //     router.push("/");
+  //   }
+  // }, [currentCourse, router]);
 
-  if (!currentCourse) {
-    return <div>Loading...</div>;
-  }
+  // if (!currentCourse) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <nav className="flex flex-col bg-sidebarColor text-white h-screen pl-3 pr-2">
       <div className="flex flex-col flex-grow">
         {/* Links */}
-        {tabList.map((it) => (
-          <Link
-            href={`/${currentCourse}${
-              it !== "Home" ? `/${it.toLocaleLowerCase()}` : ""
-            }`}
-            className={`p-4 block group border-l-4  ${
-              isActive(it) ? "border-l-4 border-white" : "border-sidebarColor"
-            }`}
-            key={it}
-          >
-            <span
-              className={`border-b-2 border-white group-hover:border-transparent ${
-                isActive(it) ? "border-b-transparent" : ""
+        {currentCourse &&
+          tabList.map((it) => (
+            <Link
+              href={`/${currentCourse}${
+                it !== "Home" ? `/${it.toLocaleLowerCase()}` : ""
               }`}
+              className={`p-4 block group border-l-4  ${
+                isActive(it) ? "border-l-4 border-white" : "border-sidebarColor"
+              }`}
+              key={it}
             >
-              {it}
-            </span>
-          </Link>
-        ))}
+              <span
+                className={`border-b-2 border-white group-hover:border-transparent ${
+                  isActive(it) ? "border-b-transparent" : ""
+                }`}
+              >
+                {it}
+              </span>
+            </Link>
+          ))}
       </div>
     </nav>
   );
